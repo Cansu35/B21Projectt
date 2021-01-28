5e6cac1d9b88416191fa60124c82c94509f67eb3
Cansu35 <71334287+Cansu35@users.noreply.github.com>
2021-01-28 07:23:37 +0100

    added new java class called Hello, added a comment into

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello");
+        //B21 is good at Intellij
+        System.out.println("B21 is good at Intellij");
     }
 }
