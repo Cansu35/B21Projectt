7023dd6943a7bf9dea1872b401baac428ae9c760
Cansu35 <71334287+Cansu35@users.noreply.github.com>
2021-01-28 08:02:01 +0100

    project is already in remote repo, committing more changes

--- src/Hello.java
+++ src/Hello.java
@@ -1,4 +1,11 @@
 public class Hello {
-//more stuff
+    //more stuff
     //Mac user and windows users are happy to see commit dialog finally
+
+    //I have already published on Github.com
+    //now i am ready to make more commits
+    //but do I need to share again
+    //absolutely no
+    //it's time commit and jsut push!!
+
 }
